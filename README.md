3daa8d3aaf6db869bb49cce54436c03eb0c34894
Etienne Dldc <e.deladonchamps@gmail.com>
2018-11-04 16:42:06 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # fish-config
+
 My fish-shell &amp; fisher config
+
+## Setup
+
+1. Install `fish`
+2. Install `fisher`
+3. Clone this repo `git clone git@github.com:etienne-dldc/fish-config.git ~/Workspace/github.com/etienne-dldc/fish-config`
+4. Create symlink to `config.fish` `ln -s ~/Workspace/github.com/etienne-dldc/fish-config/config.fish ~/.config/fish/config.fish`
+5. Create symlink to `fishfile` `ln -s ~/Workspace/github.com/etienne-dldc/fish-config/fishfile ~/.config/fish/fishfile`
+6. Run `fisher`
---
+++ config.fish
@@ -0,0 +1,91 @@
+# deno
+set PATH "/Users/etienne/.deno/bin" $PATH
+
+# cd ~/Workspace
+
+# function yarn
+#     echo "Use npm !"
+#     npm $argv
+# end
+
+function git-sync
+    git fetch $argv[1]
+    and git checkout $argv[2]
+    and git merge "$argv[1]/$argv[2]"
+end
+
+function reload
+    source ~/.config/fish/config.fish
+end
+
+# Custom tools
+function est
+    npx -p ~/Workspace/github.com/etienne-dldc/etienne-shell-tools est $argv
+end
+
+function clone
+    est clone $argv
+end
+
+function tsc
+    npx -p typescript tsc $argv
+end
+
+# Node CLI tool
+function oclif
+    npx oclif $argv
+end
+
+function npm-check
+    npx npm-check -u $argv
+end
+
+function overmind-devtools
+    npx overmind-devtools
+end
+
+function yarn-fix
+    npx yarn-tools fix-duplicates ./yarn.lock $argv >fixed-yarn.lock
+
+    get --prompt="Replace yarn.lock [yY]?:" --rule="[yY]" | read confirm
+    switch $confirm
+        case Y y
+            rm ./yarn.lock
+
+            mv ./fixed-yarn.lock ./yarn.lock
+    end
+end
+
+function cdd
+    npx captainduckduck $argv
+end
+
+function cra
+    npx create-react-app $argv
+end
+
+# function cra-ts
+#     npx create-react-app $argv --scripts-version=react-scripts-ts
+# end
+
+function cra-ts
+    npx create-react-app $argv --typescript
+    cd "./$argv[1]"
+    yarn add typescript @types/node @types/react @types/react-dom @types/jest
+end
+
+function cdd-tar
+    npx captainduckduck deploy -t ./deploy.tar $argv
+end
+
+function jss
+    npx jss $argv
+end
+
+function serve
+    npx serve $argv
+end
+
+function captainduckduck
+    npx captainduckduck $argv
+end
