be2806f1c747a32da31983fca243f37626855ae9
Etienne Dldc <e.deladonchamps@gmail.com>
2018-11-05 15:19:42 +0100

    Add nvm helper

--- config.fish
+++ config.fish
@@ -8,6 +8,10 @@ set PATH "/Users/etienne/.deno/bin" $PATH
 #     npm $argv
 # end
 
+function nvm
+    echo "=> fnm"
+end
+
 function install-est
     git clone git@github.com:etienne-dldc/etienne-shell-tools.git ~/Workspace/github.com/etienne-dldc/etienne-shell-tools
 end
